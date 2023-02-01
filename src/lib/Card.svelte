3e089ec64904933a03f2964f18f233e3c2b079b5
dragonflyinmypool <jheiney10@gmail.com>
2023-02-01 10:42:45 -0500

    attempt 1 svelte

---
+++ src/lib/Card.svelte
@@ -0,0 +1,23 @@
+<div class="card desc">
+	<slot />
+</div>
+
+<style>
+	.card {
+		padding: 25px;
+		border-radius: 30px;
+		background-color: var(--nearBlack);
+		height: 465px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.desc {
+		border: 2px solid rgb(255, 255, 255);
+	}
+
+	.prod {
+		border: 3px solid var(--purple);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+	import Card from '../lib/Card.svelte';
+	import CardProj from '../lib/CardProj.svelte';
+</script>
+
+<div class="center">
+	<main>
+		<Card>
+			<div class="top">
+				<h1>Jamie Joseph Heiney</h1>
+				1990 London
+				<ul style="margin-top: 20px;">
+					<li>HTML, CSS, Javascript, Svelte, Node</li>
+					<li>Accounting fundementals</li>
+					<li>Principals of design</li>
+				</ul>
+			</div>
+		</Card>
+		<CardProj>
+			<span slot="header">Tweet 2 Poem</span>
+			<span slot="status">Development</span>
+			<span slot="description">
+				Include this twitter bot's name in your tweet and it will automaticly reply with a poem
+				based on your tweet.
+			</span>
+			<span slot="list">
+				<ul class="bottom">
+					<li>Node.js ~ Twitter API, GPT-3 ~ Railway.app</li>
+				</ul>
+			</span>
+			<span slot="links">
+				<a href="https://github.com/dragonflyinmypool/Tweet2Poem">Repo</a>
+			</span>
+		</CardProj>
+	</main>
+</div>
