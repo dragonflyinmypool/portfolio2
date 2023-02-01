be7188ee7c8875710d8d5435f50bc7be1b75dd38
dragonflyinmypool <jheiney10@gmail.com>
2023-02-01 12:04:29 -0500

    info in

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -6,24 +6,57 @@
 	<h3>
 		{data.title}
 	</h3>
-	<span>{data.status}</span>
+	<div class="status">{data.status}</div>
 	<p>{data.description}</p>
-	<span>{data.techologies}</span>
-	<span class="links">
-		{#each data.links as link}
-			<a href={link[1]}>{link[0]}</a>
+	<div class="flex-Text small-text">
+		{#each data.techologies as tech}
+			<div class="tech">{tech}</div>
 		{/each}
-	</span>
+	</div>
+	<div class="flex-Text">
+		{#each data.links as link}
+			<div class="link"><a href={link[1]}>{link[0]}</a></div>
+		{/each}
+	</div>
 </div>
 
 <style>
 	.card {
-		padding: 1rem;
+		padding: 0.25rem 0.75rem;
+		max-width: 500px;
+		border: #00b179 2px solid;
+		border-radius: 10px;
+		height: 250px;
+		display: grid;
+		grid-template-rows: 40px 30px 85px 30px 40px;
 	}
-	.links {
+	.status {
+		line-height: 30px;
+	}
+	.flex-Text {
 		display: flex;
 		flex-direction: row;
 		justify-content: flex-start;
-		gap: 1rem;
+		gap: 0.8rem;
+		padding-top: 0.5rem;
+	}
+
+	.link {
+		border-radius: 0.7rem;
+		padding: 0.3rem 0.5rem;
+		background-color: #00b179;
+	}
+
+	.tech {
+		line-height: 1rem;
+	}
+
+	a {
+		color: white;
+		text-decoration: none;
+	}
+
+	.small-text {
+		font-size: 0.7rem;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,46 @@
 <script>
-	import people from '$lib/cardData.json';
+	import people from '$lib/data/cardData.json';
 	const projectData = people.ProductionCards;
 
 	import Card from '$lib/Card.svelte';
-	import { loop_guard } from 'svelte/internal';
+	import EmptyCard from '$lib/EmptyCard.svelte';
 </script>
 
-{#each projectData as data}
-	<Card {data} />
-{/each}
+<main>
+	<EmptyCard>
+		<h1>Jamie Joseph Heiney</h1>
+		1990 London
+		<ul style="margin-top: 20px;">
+			<li>HTML, CSS, Javascript, Svelte, Node</li>
+			<li>Accounting fundementals</li>
+			<li>Principals of design</li>
+		</ul>
+	</EmptyCard>
+	{#each projectData as data}
+		<Card {data} />
+	{/each}
+	<EmptyCard>
+		<h2>Courses</h2>
+		<ul style="list-style:disc">
+			<li>Intermediate JavaScript</li>
+			<li>Understanding the Weird Parts</li>
+			<li>JavaScript Algorithms and Data Structures</li>
+			<li>Svelte.js, The Complete Guide by Max Schwarzmüller, Udemy</li>
+			<li>
+				NodeJS, The Complete Guide (MVC, REST APIs, GraphQL, Denode) by Max Schwarzmüller, Udemy
+			</li>
+			<li>The Complete Google Sheets Course: Beginner to Advanced!</li>
+		</ul>
+	</EmptyCard>
+</main>
+
+<style>
+	main {
+		max-width: 1200px;
+		margin: auto;
+		width: initial;
+		display: grid;
+		gap: 1rem;
+		grid-template-columns: repeat(auto-fit, minmax(325px, 1fr));
+	}
+</style>
