f4a2d5c6807e9f363ec7677487daad7b628fe72f
dragonflyinmypool <jheiney10@gmail.com>
2023-02-01 11:20:32 -0500

    svelte good baby

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -9,11 +9,21 @@
 	<span>{data.status}</span>
 	<p>{data.description}</p>
 	<span>{data.techologies}</span>
-	<span>{data.links}</span>
+	<span class="links">
+		{#each data.links as link}
+			<a href={link[1]}>{link[0]}</a>
+		{/each}
+	</span>
 </div>
 
 <style>
 	.card {
 		padding: 1rem;
+	}
+	.links {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		gap: 1rem;
 	}
 </style>
